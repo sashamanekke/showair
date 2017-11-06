1d038a361b4db138b1b1401ba571bed3f07cf053
sasha de laet <sasha.delaet@gmail.com>
2017-11-06 16:31:44 +0100

    add controllers

---
+++ app/controllers/showers_controller.rb
@@ -0,0 +1,22 @@
+class ShowersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  def new
+  end
+  def create
+
+  end
+
+  def edit
+  end
+  def update
+
+  end
+end
