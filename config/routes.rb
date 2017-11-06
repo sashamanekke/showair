3b2bc68fc64e48d354bfc9c88d4f931a3d3e8063
sasha de laet <sasha.delaet@gmail.com>
2017-11-06 16:18:46 +0100

    routes definition

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :showers do
+    resources :reservations, only: [:show, :create, :new]
+  end
+  resources :users, only: [:show]
 end
+
+# show
+# create
+# new
+# edit
+# update
+# index
+# destroy
