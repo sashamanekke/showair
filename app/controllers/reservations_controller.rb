19150313e7a4b785597b0af11ca190e3297c8947
Jun Joseph Padilla <jun.joseph.padilla@gmail.com>
2017-11-08 14:59:06 +0100

    Show 1 page good, goes to payment page

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,10 +1,29 @@
 class ReservationsController < ApplicationController
+  # before_action :set_reservation, only: [:new, :create]
+
   def show
   end
 
   def new
+    # @reservation = Reservation.new
   end
 
   def create
+    # @reservation = Reservation.new(reservation_params)
+    # @reservation.shower = @shower
+    # if @reservation.save
+    #     redirect_to shower_path(@shower)
+    #   else
+    #     render :new
+    # end
   end
+
+  # private
+  # def set_reservation
+  #   @shower = Shower.find(params[:shower_id])
+  # end
+  # def reservation_params
+  #   params.require(:reservation).permit(:hour)
+  # end
+
 end
