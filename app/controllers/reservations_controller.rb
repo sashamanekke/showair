90d843297d46f8753667e0e9aae3b328fd65e934
Jun Joseph Padilla <jun.joseph.padilla@gmail.com>
2017-11-12 22:00:43 +0100

    updated reservations controller - removed comments and added :show to before_action

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,6 +1,6 @@
 class ReservationsController < ApplicationController
 
-  before_action :set_reservation, only: [:new, :create]
+  before_action :set_reservation, only: [:new, :create, :show]
 
   def index
     @reservations = Reservation.all
@@ -42,31 +42,5 @@ class ReservationsController < ApplicationController
     params.require(:reservation).permit(:hour, :availibility)
   end
 end
-
-
-# 1) when you create a new reservation - it needs to find the times available (:availability) - in AvailabilityController, it's defined by finding the shower id in def set_reservation, then -->
-# @availibility = Availibility.find(reservation_params[:availibility])
-
-# 2) then it needs the reservation time that was selected -->
-# @reservation = Reservation.new(reservation_params[:hour])
-
-# 3) reservation id = shower.id
-
-# 4) then reservation is set to the hour chosen -->
-# @reservation.hour = @availibility.hour
-
-# 5) then that chosen hour needs to be set to false  and saved -->
-    # @availibility.available = false
-    # @availibility.save
-# 6) then the user who has the reservation is set to current_user -->
-    # @reservation.user = current_user
-
-# 7) then if it saves, it'll go to the resrevation page. Otherwise, it'll reload the page -->
-    # if @reservation.save
-    #     redirect_to shower_reservation_path(@shower, @reservation)
-    #   else
-    #     render :new
-    # end
-
 
 
